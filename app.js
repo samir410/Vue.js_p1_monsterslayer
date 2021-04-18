deb44a400ce6686f4dc88625bd034b98a8e8948a
Samir Hossain <samirhossain924@gmail.com>
2021-04-18 12:26:30 +0600

    attack and special attack add

--- app.js
+++ app.js
@@ -10,6 +10,55 @@ new Vue({
             this.playerHealth =100;
             this.monsterHealth = 100;
             this.gameRuning =true;
+        },
+        attack: function(){
+           this.monsterHealth-=this.calculateDamage(10, 3);
+           if(this.checkWin()){
+               return true;
+           }
+          this.monsterAttack();
+        },
+        specialAttack: function(){
+           this.monsterHealth-=this.calculateDamage(10, 3);
+           if(this.checkWin()){
+               return true;
+           }
+           this.monsterAttack();
+        },
+        heal: function(){
+
+        },
+        giveUp:function(){
+
+        },
+        monsterAttack:function(){
+            this.playerHealth-=this.calculateDamage(12, 5);
+            this.checkWin();
+        },
+        calculateDamage:function(max , min){
+           return Math.max(Math.floor(Math.random() * max)+1, min) ;
+        },
+        checkWin:function(){
+            if(this.monsterHealth<=0){
+                if(confirm('you won! New game?')){
+                    this.startGame();
+                }
+                else{
+                    this.gameRuning = false;
+                }
+            return true;
+            }
+            else if(this.playerHealth<=0){
+                if(confirm('Monster won! New game?')){
+                    this.startGame();
+                }
+                else{
+                    this.gameRuning = false;
+                }
+            return true;
+            }
+        return false;
         }
+
     }
 })
